eb3fc9470e1a0da1dfe7549a530e53326f8e2441
Federica Macchi <federica.macchi3@gmail.com>
2019-07-13 17:58:48 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(namesArr, event) {
+    let thanksArr = [];
+    for (let i = 0; i < namesArr.length; i++) {
+        thanksArr.push(`Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanksArr
+}
+
+function countdown(num) {
+    while (num > -1) {
+        console.log(num--)
+    }
+}
